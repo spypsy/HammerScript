15822f65c3de3e87459c835f08a21405dfa52b8c
spypsy <spypsy@users.noreply.github.com>
2015-09-03 16:03:38 +0100

    Update hammerscript.js

--- hammerscript.js
+++ hammerscript.js
@@ -1,7 +1,7 @@
 var body = document.getElementsByTagName('body')[0];
 var mc = new Hammer(body);
 mc.on("panleft panright", function(ev) {
-    console.log(ev.type +" gesture detected.");
+    console.log(ev.type +" gesture detected madafaka.");
     console.log(ev);
 });
 
