b2568e6ce57da1afda96b4f2f96490f171cbf51a
spypsy <spypsy@users.noreply.github.com>
2015-09-03 15:13:56 +0100

    Update hammerscript.js

--- hammerscript.js
+++ hammerscript.js
@@ -3,3 +3,5 @@ var mc = new Hammer(body);
 mc.on("panleft panright", function(ev) {
     console.log(ev.type +" gesture detected.");
 });
+body.addEventListener('mousemove', console.log('mouse moved'));
+body.addEventListener('touchmove', console.log('touch moved'));
